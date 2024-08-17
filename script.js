f896bb5bf807ae3829fa77feb94ca2830f1fdc7e
Sonali Saxena <sonalisaxena3112@gmail.com>
2024-08-17 13:31:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let myDiv = document.getElementById("my-Div");
+
+document.addEventListener("mousemove", (e) => {
+    move(e);
+});
+
+const move = (e) => {
+    var x = e.pageX;
+    var y = e.pageY;
+    myDiv.style.left = x - 100 + "px";
+    myDiv.style.top = y - 100 + "px"; 
+};
